28d21e107e5b21b66a119e3c59da4aeec3bd024e
Tom Dillon <t-dillon@users.noreply.github.com>
2019-07-10 14:17:53 -0700

    standardized benchmark script

---
+++ benchmarks/bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+(
+    echo "###########################################"
+    echo "# BUILD INFO"
+    echo "###########################################"
+    build/run_benchmark -h | grep build.info
+
+    echo
+    echo "###########################################"
+    echo "# CPU INFO"
+    echo "###########################################"
+    lscpu
+
+    echo
+    echo "###########################################"
+    echo "# BENCHMARK"
+    echo "###########################################"
+    set -x
+    taskset 0x20 build/run_benchmark -t30 -t10 -n500000 -sfsss2,fsss2:1,jczsolve,skbforce,tdoku data/*
+
+) | tee $1
